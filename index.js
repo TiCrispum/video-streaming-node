e5b4c421f3cabfe930199db7c4978b8188d9538f
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2021-12-30 19:26:48 +0100

    Add controller to stream video from local storage to path "video"

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express')
+const fs = require("fs");
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/video', (req, res) => {
+    const path = "../videos/SampleVideo_1280x720_1mb.mp4";
+    fs.stat(path, (err, stats) => {
+        if (err) {
+            console.error("An error occurred");
+            res.sendStatus(500);
+            return;
+        }
+
+        res.writeHead(200, {
+            "Content-Length": stats.size,
+            "Content-Type": "video/mp4",
+        });
+
+        fs.createReadStream(path).pipe(res);
+    })
 })
 
 app.listen(port, () => {
