17dd81b117a7d8380f95673275a3219f8fd12f79
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-03-06 14:25:14 +0100

    Add mongodb to store video metadata and provide stronger separation of concerns

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const http = require("http");
+const mongodb = require("mongodb");
 
 const app = express()
 
@@ -10,26 +11,57 @@ if (!process.env.PORT) {
 const PORT = process.env.PORT;
 const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
 const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
+const DBHOST = process.env.DBHOST;
+const DBNAME = process.env.DBNAME;
 
-app.get('/video', (req, res) => {
-    const forwardRequest = http.request(
-        {
-            host: VIDEO_STORAGE_HOST,
-            port: VIDEO_STORAGE_PORT,
-            path: '/video?path=SampleVideo_1280x720_1mb.mp4',
-            method: 'GET',
-            headers: req.headers
-        },
-        forwardResponse => {
-            console.log('Forwarding video requests to ' + VIDEO_STORAGE_HOST + ':' + VIDEO_STORAGE_PORT)
-            res.writeHead(forwardResponse.statusCode, forwardResponse.headers);
-            forwardResponse.pipe(res);
-        }
-    );
+function main() {
 
-    req.pipe(forwardRequest);
-});
+    return mongodb.MongoClient.connect(DBHOST)
+        .then(client => {
+            const db = client.db(DBNAME);
+            const videosCollection = db.collection("videos");
 
-app.listen(PORT, () => {
-    console.log(`Video Streaming Microservice is Up and Running!`)
-})
+            app.get('/video', (req, res) => {
+                const videoId = new mongodb.ObjectID(req.query.id);
+                videosCollection.findOne({_id: videoId})
+                    .then(videoRecord => {
+                        if (!videoRecord) {
+                            res.sendStatus(404);
+                            return;
+                        }
+
+                        const forwardRequest = http.request(
+                            {
+                                host: VIDEO_STORAGE_HOST,
+                                port: VIDEO_STORAGE_PORT,
+                                path: `/video?path=${videoRecord.videoPath}`,
+                                method: 'GET',
+                                headers: req.headers
+                            },
+                            forwardResponse => {
+                                console.log('Forwarding video requests to ' + VIDEO_STORAGE_HOST + ':' + VIDEO_STORAGE_PORT)
+                                res.writeHead(forwardResponse.statusCode, forwardResponse.headers);
+                                forwardResponse.pipe(res);
+                            }
+                        );
+                        req.pipe(forwardRequest);
+                    })
+                    .catch(err => {
+                        console.error("Database query failed.");
+                        console.error(err && err.stack || err);
+                        res.sendStatus(500);
+                    });
+            });
+
+            app.listen(PORT, () => {
+                console.log(`Video Streaming Microservice is Up and Running!`)
+            });
+        });
+}
+
+main()
+    .then(() => console.log("Video Streaming Microservice is Up and Running!"))
+    .catch(err => {
+        console.error("Video Streaming Microservice failed to start.");
+        console.error(err && err.stack || err);
+    })
