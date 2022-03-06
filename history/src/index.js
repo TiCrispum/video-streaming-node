b62a2609015f7d2dd1720eafd9ed91026a1bbd4c
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-03-06 22:17:05 +0100

    Add history service

---
+++ history/src/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+function setupHandlers(app) {
+
+}
+
+function startHttpServer() {
+    return new Promise(resolve => {
+        const app = express();
+        setupHandlers(app);
+
+        const port = process.env.PORT && parseInt(process.env.PORT) || 3000;
+        app.listen(port, () => {
+            resolve();
+        });
+    });
+}
+
+function main() {
+    console.log("Hello world!");
+
+    return startHttpServer();
+}
+
+main()
+    .then(() => console.log("Microservice online."))
+    .catch(err => {
+        console.error("Microservice failed to start.");
+        console.error(err && err.stack || err);
+    });
