c02cf64df5719e1297ab1b6ee2048c7c01373552
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-05-20 16:41:10 +0100

    Add mongodb and rabbitmq to stub

--- recommendations/src/index.js
+++ recommendations/src/index.js
@@ -1,5 +1,42 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongodb = require("mongodb");
+const amqp = require('amqplib');
+
+if (!process.env.DBHOST) {
+    throw new Error("Please specify the databse host using environment variable DBHOST.");
+}
+
+if (!process.env.DBNAME) {
+    throw new Error("Please specify the name of the database using environment variable DBNAME");
+}
+
+if (!process.env.RABBIT) {
+    throw new Error("Please specify the name of the RabbitMQ host using environment variable RABBIT");
+}
+
+const DBHOST = process.env.DBHOST;
+const DBNAME = process.env.DBNAME;
+const RABBIT = process.env.RABBIT;
+
+function connectDb() {
+    return mongodb.MongoClient.connect(DBHOST)
+        .then(client => {
+            return client.db(DBNAME);
+        });
+}
+
+function connectRabbit() {
+
+    console.log(`Connecting to RabbitMQ server at ${RABBIT}.`);
+
+    return amqp.connect(RABBIT)
+        .then(messagingConnection => {
+            console.log("Connected to RabbitMQ.");
+
+            return messagingConnection.createChannel();
+        });
+}
 
 function startHttpServer() {
     return new Promise(resolve => {
@@ -14,7 +51,13 @@ function startHttpServer() {
 }
 
 function main() {
-    return startHttpServer();
+    return connectDb()
+        .then(db => {
+            return connectRabbit()
+                .then(messageChannel => {
+                    return startHttpServer(db, messageChannel);
+                });
+        });
 }
 
 main()
