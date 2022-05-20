a0e6c3983ea80cf2bb3d30c89c5c2bf744995e58
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-05-20 14:35:27 +0100

    Code refactoring for video-streaming

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -20,56 +20,71 @@ const DBHOST = process.env.DBHOST;
 const DBNAME = process.env.DBNAME;
 const RABBIT = process.env.RABBIT;
 
-function main() {
-
+function connectDb() {
     return mongodb.MongoClient.connect(DBHOST)
         .then(client => {
-            amqp.connect(RABBIT)
-                .then(messagingConnection => {
-
-                    const db = client.db(DBNAME);
-                    const videosCollection = db.collection("videos");
-
-                    app.get('/video', (req, res) => {
-                        const videoId = new mongodb.ObjectID(req.query.id);
-                        videosCollection.findOne({_id: videoId})
-                            .then(videoRecord => {
-                                if (!videoRecord) {
-                                    res.sendStatus(404);
-                                    return;
-                                }
-
-                                sendViewedMessage(messagingConnection, videoRecord.videoPath);
-
-                                const forwardRequest = http.request(
-                                    {
-                                        host: VIDEO_STORAGE_HOST,
-                                        port: VIDEO_STORAGE_PORT,
-                                        path: `/video?path=${videoRecord.videoPath}`,
-                                        method: 'GET',
-                                        headers: req.headers
-                                    },
-                                    forwardResponse => {
-                                        console.log('Forwarding video requests to ' + VIDEO_STORAGE_HOST + ':' + VIDEO_STORAGE_PORT)
-                                        res.writeHead(forwardResponse.statusCode, forwardResponse.headers);
-                                        forwardResponse.pipe(res);
-                                    }
-                                );
-                                req.pipe(forwardRequest);
-                            })
-                            .catch(err => {
-                                console.error("Database query failed.");
-                                console.error(err && err.stack || err);
-                                res.sendStatus(500);
-                            });
-                    });
-
-                    app.listen(PORT, () => {
-                        console.log(`Video Streaming Microservice is Up and Running!`)
-                    });
-
-                });
+            return client.db(DBNAME);
         });
+}
+
+function connectRabbit() {
+    return amqp.connect(RABBIT)
+        .then(messagingConnection => {
+            return messagingConnection.createChannel();
+        });
+}
+
+function startHttpServer(db, messageChannel) {
+    const videosCollection = db.collection("videos");
+
+    app.get('/video', (req, res) => {
+        const videoId = new mongodb.ObjectID(req.query.id);
+        videosCollection.findOne({_id: videoId})
+            .then(videoRecord => {
+                if (!videoRecord) {
+                    res.sendStatus(404);
+                    return;
+                }
+
+                sendViewedMessage(messageChannel, videoRecord.videoPath);
+
+                const forwardRequest = http.request(
+                    {
+                        host: VIDEO_STORAGE_HOST,
+                        port: VIDEO_STORAGE_PORT,
+                        path: `/video?path=${videoRecord.videoPath}`,
+                        method: 'GET',
+                        headers: req.headers
+                    },
+                    forwardResponse => {
+                        console.log('Forwarding video requests to ' + VIDEO_STORAGE_HOST + ':' + VIDEO_STORAGE_PORT)
+                        res.writeHead(forwardResponse.statusCode, forwardResponse.headers);
+                        forwardResponse.pipe(res);
+                    }
+                );
+                req.pipe(forwardRequest);
+            })
+            .catch(err => {
+                console.error("Database query failed.");
+                console.error(err && err.stack || err);
+                res.sendStatus(500);
+            });
+    });
+
+    app.listen(PORT, () => {
+        console.log(`Video Streaming Microservice is Up and Running!`)
+    });
+}
+
+function main() {
+
+    return connectDb()
+        .then(db => {
+            return connectRabbit()
+                .then(messageChannel => {
+                    return startHttpServer(db, messageChannel);
+                });
+        })
 }
 
 function sendViewedMessage(messageChannel, videoPath) {
