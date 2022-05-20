ac7f2287841d9ef8ec7ace0813ac536eb1074698
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-05-20 11:39:33 +0100

    Update the history service to receive and save watched videos

--- history/src/index.js
+++ history/src/index.js
@@ -1,13 +1,48 @@
 const express = require("express");
+const mongodb = require("mongodb");
+const bodyParser = require("body-parser");
 
-function setupHandlers(app) {
-
+if (!process.env.DBHOST) {
+    throw new Error("Please specify the databse host using environment variable DBHOST.");
 }
 
-function startHttpServer() {
+if (!process.env.DBNAME) {
+    throw new Error("Please specify the name of the database using environment variable DBNAME");
+}
+
+const DBHOST = process.env.DBHOST;
+const DBNAME = process.env.DBNAME;
+
+function setupHandlers(app, db) {
+    const videosCollection = db.collection("videos");
+
+    app.post("/viewed", (req, res) => {
+        const videoPath = req.body.videoPath;
+        videosCollection
+            .insertOne({ videoPath: videoPath })
+            .then(() => {
+                console.log(`Added video ${videoPath} to history.`);
+            })
+            .catch(err => {
+                console.error(`Error adding video ${videoPath} to history.`);
+                console.error(err && err.stack || err);
+                res.sendStatus(500);
+            })
+    })
+}
+
+function connectDb() {
+    return mongodb.MongoClient.connect(DBHOST)
+        .then(client => {
+            return client.db(DBNAME);
+        });
+}
+
+function startHttpServer(db) {
     return new Promise(resolve => {
         const app = express();
-        setupHandlers(app);
+        app.use(bodyParser.json());
+        setupHandlers(app, db);
 
         const port = process.env.PORT && parseInt(process.env.PORT) || 3000;
         app.listen(port, () => {
@@ -19,7 +54,11 @@ function startHttpServer() {
 function main() {
     console.log("Hello world!");
 
-    return startHttpServer();
+    return connectDb()
+        .then(db => {
+            return startHttpServer(db);
+        })
+
 }
 
 main()
