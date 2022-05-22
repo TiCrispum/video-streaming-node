4e4cd29301180bdec5ea8d3298de8d2bfcfc3103
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-05-22 13:31:07 +0100

    Adding kubernetes provider

--- scripts/providers.tf
+++ scripts/providers.tf
@@ -4,3 +4,13 @@ provider "azurerm" {
 
 provider "tls" {
 }
+
+provider "kubernetes" {
+  host = azurerm_kubernetes_cluster.cluster.kube_config[0].host
+
+  client_certificate = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].client_certificate)
+
+  client_key = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].client_key)
+
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate)
+}
