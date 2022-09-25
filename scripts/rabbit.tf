926ca971a1ae3e3ae94285fc31d33fc493136fc0
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-09-25 13:31:44 +0100

    Add rabbitmq to kubernetes with LoadBalancer for the dashboard

---
+++ scripts/rabbit.tf
@@ -0,0 +1,74 @@
+# Deploys a RabbitMQ server to the Kubernetes cluster.
+
+resource "kubernetes_deployment" "rabbit" {
+  metadata {
+    name = "rabbit"
+
+    labels = {
+      pod = "rabbit"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        pod = "rabbit"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          pod = "rabbit"
+        }
+      }
+
+      spec {
+        container {
+          image = "3.10.1-management"
+          name  = "rabbit"
+
+          port {
+            container_port = 5672
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "rabbit" {
+    metadata {
+        name = "rabbit"
+    }
+
+    spec {
+        selector = {
+            pod = kubernetes_deployment.rabbit.metadata[0].labels.pod
+        }   
+
+        port {
+            port        = 5672
+        }
+    }
+}
+
+resource "kubernetes_service" "rabbit_dashboard" {
+    metadata {
+        name = "rabbit-dashboard"
+    }
+
+    spec {
+        selector = {
+            pod = kubernetes_deployment.rabbit.metadata[0].labels.pod
+        }   
+
+        port {
+            port        = 15672
+        }
+
+        type             = "LoadBalancer"
+    }
+}
